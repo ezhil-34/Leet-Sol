95970c0479a80bb71d78cff1e5658e42d7db8998
ezhil-34 <ezhilmayilvaganan393@gmail.com>
2025-06-27 17:01:58 +0530

    Time: 199 ms (86.79%) | Memory: 44.6 MB (98.11%) - LeetSync

--- 2140-longest-subsequence-repeated-k-times/longest-subsequence-repeated-k-times.java
+++ 2140-longest-subsequence-repeated-k-times/longest-subsequence-repeated-k-times.java
@@ -1,28 +1,59 @@
 class Solution {
     public String longestSubsequenceRepeatedK(String s, int k) {
-        String r="";
-        Queue<String> q=new LinkedList<>();
-        for(q.add("");!q.isEmpty();) {
-            String c=q.poll();
-            for(char ch='a';ch<='z';ch++) {
-                String n=c+ch;
-                if(isK(n,s,k)) {
-                    r=n;
-                    q.add(n);
+        char[] ca = s.toCharArray();
+        int n = ca.length;
+
+        int[] freq = new int[26];
+        for (int i = 0; i < n; i++)
+            ++freq[ca[i] - 'a'];
+
+        // all candidate subsequences grouped by length (1 to 7)
+        ArrayList<String>[] cand = new ArrayList[8];
+        cand[1] = new ArrayList<>();
+        String ans = "";
+
+        // Initialize 1-length candidates
+        for (int i = 0; i < 26; i++) {
+            if (freq[i] >= k) {
+                String str = "" + (char) ('a' + i);
+                cand[1].add(str);
+                ans = str;  // At least one valid candidate
+            }
+        }
+
+        // Build candidates of length 2 to 7
+        for (int i = 2; i < 8; i++) {
+            cand[i] = new ArrayList<>();
+            for (String prev : cand[i - 1]) {
+                for (String c : cand[1]) {
+                    String next = prev + c;
+                    if (isKRepeatedSubseq(s, next, k)) {
+                        cand[i].add(next);
+                        // Update answer if longer or lexicographically larger
+                        if (next.length() > ans.length() ||
+                            (next.length() == ans.length() && next.compareTo(ans) > 0)) {
+                            ans = next;
+                        }
+                    }
                 }
             }
         }
-        return r;
+        return ans;
     }
-    boolean isK(String s,String t,int k) {
-        int c=0,i=0;
-        for(char ch:t.toCharArray()) {
-            if(ch==s.charAt(i)) {
-                if(++i==s.length()) {
-                    i=0;
-                    if(++c==k) return true;
+
+    // Checks if (target * k) is a subsequence of s
+    private boolean isKRepeatedSubseq(String s, String target, int k) {
+        int i = 0, j = 0, count = 0;
+        while (i < s.length()) {
+            if (s.charAt(i) == target.charAt(j)) {
+                j++;
+                if (j == target.length()) {
+                    count++;
+                    if (count == k) return true;
+                    j = 0;
                 }
             }
+            i++;
         }
         return false;
     }
