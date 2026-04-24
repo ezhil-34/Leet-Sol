a1442972b31aa9e2480867e7876405cbb72e3521
Ezhil Mayil Vaganan S <ezhilmayilvaganan393@gmail.com>
2026-04-24 18:37:54 +0530

    Time: 18 ms (80.53%) | Memory: 50.8 MB (46.45%) - LeetSync

--- 85-maximal-rectangle/maximal-rectangle.java
+++ 85-maximal-rectangle/maximal-rectangle.java
@@ -1,37 +1,43 @@
 class Solution {
     public int maximalRectangle(char[][] matrix) {
-        int[] arr = new int[matrix[0].length];
+        
+        if(matrix.length == 0) return 0;
+        int col = matrix[0].length;
         int max = 0;
-        for(int i = 0;i<matrix.length;i++){
-            for(int j =0;j<matrix[0].length;j++){
-                if(matrix[i][j]== '1'){
-                    arr[j]++;
-                }
-                else{
-                    arr[j] = 0;
-                }
+        int[] heights = new int[col];
+        
+        for(char[] rows : matrix){
 
+            for(int i = 0;i<col;i++){
 
+                heights[i] = (rows[i] == '1') ? heights[i]+1:0;
             }
-            max = Math.max(max,his(arr));
-        }
-        return max;
-    }
-    public int his(int[] arr){
 
-    
-         Stack<Integer> st = new Stack<>();
+            max = Math.max(max,largest(heights));
+        }
+
+        return max;
+
+    }
+
+    public int largest(int[] heights){
+
+        Stack<Integer> st = new Stack();
+
         int max = 0;
-        int n = arr.length;
-        for(int i = 0;i<=n;i++){
-            int h = (i==n)? 0 : arr[i];
-            while(!st.isEmpty() && h<arr[st.peek()]){
-                int hei = arr[ st.pop()];
-                int wid = st.isEmpty() ? i:i - st.peek() -1;
-                 max = Math.max(max,hei*wid);
+
+        for(int i = 0;i<=heights.length;i++){
+            int curr = (i == heights.length) ?  0: heights[i];
+
+            while(!st.isEmpty() && curr < heights[st.peek()]){
+                int h = heights[st.pop()];
+                int w = st.isEmpty() ? i : i - st.peek() - 1;
+                max = Math.max(max,h*w);
+
 
             }
             st.push(i);
+
         }
         return max;
     }
