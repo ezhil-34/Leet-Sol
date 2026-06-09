ed2a82113762f71a8ef85d148f3ff32d0f756a70
Ezhil Mayil Vaganan S <ezhilmayilvaganan393@gmail.com>
2026-06-09 07:48:12 +0530

    Time: 14 ms (92.40%) | Memory: 43.4 MB (88.74%) - LeetSync

--- 5-longest-palindromic-substring/longest-palindromic-substring.java
+++ 5-longest-palindromic-substring/longest-palindromic-substring.java
@@ -1,30 +1,29 @@
 class Solution {
     public String longestPalindrome(String s) {
-       if(s.length() < 1 && s == null) return "";
-       int n = s.length();
-        int st = 0, ed = 0;
+       if(s.length() == 0) return "";
+       int st = 0, ed = 0;
 
-        for(int i =0;i<n;i++){
-            int len1 = extractfromcenter(s,i,i);
-            int len2 = extractfromcenter(s,i,i+1);
+       for(int i =0;i<s.length();i++){
+
+            int len1 = extract(s,i,i);
+            int len2 = extract(s,i,i+1);
+
             int len = Math.max(len1,len2);
 
-            if(len > ed-st){
-                st = i - (len-1)/2;
-                ed = i + len/2;
+            if(len>ed-st){
+                 st = i - (len-1)/2;
+                 ed = i+ (len)/2;
             }
-
-            
-
-        }
-        return s.substring(st,ed+1);
+       }
+       return s.substring(st,ed+1);
     }
 
-    public int extractfromcenter(String s , int left,int right){
+    public int extract(String s, int left, int right){
+
         while(left>=0 && right<s.length() && s.charAt(left) == s.charAt(right)){
             left--;
             right++;
         }
-        return right - left - 1;
+        return right-left-1;
     }
 }
