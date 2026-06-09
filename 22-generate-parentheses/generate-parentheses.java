273e505a440d9bc40574a9a2032c4dd4f58f07ef
Ezhil Mayil Vaganan S <ezhilmayilvaganan393@gmail.com>
2026-06-09 20:00:28 +0530

    Time: 0 ms (100.00%) | Memory: 44.1 MB (92.61%) - LeetSync

--- 22-generate-parentheses/generate-parentheses.java
+++ 22-generate-parentheses/generate-parentheses.java
@@ -1,31 +1,30 @@
 class Solution {
     public List<String> generateParenthesis(int n) {
-        
-        StringBuilder sb = new StringBuilder();
-        List<String> res = new ArrayList<>();
+      
+      StringBuilder sb = new StringBuilder();
+      List<String> res = new ArrayList<>();
 
-        backtracking(sb, res, 0, 0, n);
+      dfs(sb,res,0,0,n);
+      return res;
 
-        return res;
     }
+    public void dfs(StringBuilder sb, List<String> res,int open,int close,int n){
+        if(open == close && open == n){
+            res.add(sb.toString());
+            return;
+        }
 
-    void backtracking(StringBuilder sb, List<String> res, int open, int close, int n)
-    {
-        if(open == close && open == n) { res.add(sb.toString()); return; }
-
-        if(open < n) 
-        {
+        if(open<n){
             sb.append("(");
-            backtracking(sb, res, open + 1, close, n);
-            sb.deleteCharAt(sb.length() -1 );
+            dfs(sb,res,open+1,close,n);
+            sb.deleteCharAt(sb.length()-1);
         }
 
-        if(close < open)
-        {
+        if(close<open){
             sb.append(")");
-            backtracking(sb, res, open, close +1, n);
-            sb.deleteCharAt(sb.length() - 1);
+            dfs(sb,res,open,close+1,n);
+            sb.deleteCharAt(sb.length()-1);
         }
-
+        return;
     }
 }
