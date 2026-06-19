fb74ca2179ce329a08c53ed9b7fa617bc7993df3
Ezhil Mayil Vaganan S <ezhilmayilvaganan393@gmail.com>
2026-06-19 20:11:30 +0530

    Time: 580 ms (65.77%) | Memory: 0B (100.00%) - LeetSync

---
+++ 585-investments-in-2016/investments-in-2016.sql
@@ -0,0 +1,11 @@
+# Write your MySQL query statement below
+SELECT ROUND(SUM(tiv_2016),2) as tiv_2016 
+    FROM Insurance WHERE tiv_2015 in (
+        SELECT tiv_2015 FROM Insurance Group by 
+            tiv_2015 HAVING COUNT(*) >1
+    )
+    AND (lat,lon) IN (
+        SELECT lat,lon FROM Insurance 
+            GROUP BY lat,lon
+                HAVING COUNT(*) =1
+    );
