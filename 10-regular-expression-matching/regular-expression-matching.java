20d55239cbc3b03a33569fb777092b76a3246a6c
Ezhil Mayil Vaganan S <ezhilmayilvaganan393@gmail.com>
2026-07-09 07:34:46 +0530

    Time: 2 ms (49.14%) | Memory: 43.5 MB (48.27%) - LeetSync

--- 10-regular-expression-matching/regular-expression-matching.java
+++ 10-regular-expression-matching/regular-expression-matching.java
@@ -1,19 +1,20 @@
 class Solution {
     public boolean isMatch(String s, String p) {
-       int m = s.length();
-       int n = p.length();
+        int m = s.length();
+        int n = p.length();
 
-       boolean[][] dp = new boolean[m+1][n+1];
+        boolean[][] dp = new boolean[m+1][n+1];
 
         dp[0][0] = true;
-        for(int i = 2;i<=n;i++){
-            if(p.charAt(i-1) == '*'){
-                dp[0][i] = dp[0][i-2];
+
+        for(int j = 2;j<=n;j++){
+            if(p.charAt(j-1) == '*'){
+                dp[0][j] = dp[0][j-2];
             }
         }
 
         for(int i = 1;i<=m;i++){
-            for(int j = 1;j<=n;j++){
+            for(int j =1;j<=n;j++){
 
                 if(p.charAt(j-1) == '.' || s.charAt(i-1) == p.charAt(j-1)){
                     dp[i][j] = dp[i-1][j-1];
@@ -22,12 +23,14 @@ class Solution {
                 else if(p.charAt(j-1) == '*'){
                     dp[i][j] = dp[i][j-2];
 
-                    if(p.charAt(j-2) == '.' || p.charAt(j-2) == s.charAt(i-1)){
-                        dp[i][j] = dp[i][j]||dp[i-1][j];
+                    if(p.charAt(j-2) == '.' || p.charAt(j-2)==s.charAt(i-1)){
+                        dp[i][j] |= dp[i-1][j];
                     }
                 }
             }
         }
+
         return dp[m][n];
+       
     }
 }
