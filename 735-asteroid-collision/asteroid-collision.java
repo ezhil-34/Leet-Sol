f51b8b13d23ab7570b9d60953f2b86bca5816171
Ezhil Mayil Vaganan S <ezhilmayilvaganan393@gmail.com>
2026-07-28 22:57:21 +0530

    Time: 4 ms (83.00%) | Memory: 46.9 MB (59.15%) - LeetSync

--- 735-asteroid-collision/asteroid-collision.java
+++ 735-asteroid-collision/asteroid-collision.java
@@ -1,13 +1,15 @@
 class Solution {
     public int[] asteroidCollision(int[] asteroids) {
-        
-        Stack<Integer> st = new Stack<>();
+        Stack<Integer> st =  new Stack<>();
+
         for(int n : asteroids){
             boolean alive = true;
-            while(alive && n<0&& !st.isEmpty() && st.peek()>0){
-                if(st.peek() < -n){
+
+            while(alive && n<0 && !st.isEmpty() && st.peek()>0){
+                if(st.peek() < - n){
                     st.pop();
                 }
+
                 else if(st.peek() == -n){
                     st.pop();
                     alive = false;
@@ -20,10 +22,12 @@ class Solution {
                 st.push(n);
             }
         }
+
         int[] res = new int[st.size()];
-        for(int i = res.length -1;i>=0;i-- ){
+        for(int i = st.size()-1;i>=0;i--){
             res[i] = st.pop();
         }
+
         return res;
     }
 }
