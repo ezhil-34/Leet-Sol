15472c45fd1e0d6d3dd1ce06e53f1d75a8f3a02a
Ezhil Mayil Vaganan S <ezhilmayilvaganan393@gmail.com>
2026-07-31 07:21:10 +0530

    Time: 6 ms (88.76%) | Memory: 51.8 MB (39.94%) - LeetSync

---
+++ 1020-longest-turbulent-subarray/longest-turbulent-subarray.java
@@ -0,0 +1,23 @@
+class Solution {
+    public int maxTurbulenceSize(int[] arr) {
+       int up = 1, down =1,max=1;
+
+       for(int i =1;i<arr.length;i++){
+            if(arr[i]>arr[i-1]){
+                up =down+1;
+                down = 1;
+            }
+            else if(arr[i]<arr[i-1]){
+                down=up+1;
+                up = 1;
+            }
+            else{
+                up = down = 1;
+            }
+
+            max = Math.max(max,Math.max(up,down));
+       }
+
+       return max;
+    }
+}
