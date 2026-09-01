dea4bc2a5a69ec87b0f4b93c6dc892c05d97dce7
Ezhil Mayil Vaganan S <ezhilmayilvaganan393@gmail.com>
2026-09-01 18:26:03 +0530

    Time: 0 ms (100.00%) | Memory: 42.9 MB (41.75%) - LeetSync

--- 213-house-robber-ii/house-robber-ii.java
+++ 213-house-robber-ii/house-robber-ii.java
@@ -1,33 +1,27 @@
 class Solution {
     public int rob(int[] nums) {
-        int n = nums.length;
-        int[] num1 = new int[n-1];
-        int[] num2 = new int[n-1];
-        if(n==1) return nums[0];
-        int j =0,k =0;
-        for(int i =0;i<n;i++){
-            if(i!=0) num2[j++] = nums[i];
-            if(i!=n-1) num1[k++] = nums[i];
-        }
-        int ans1 = sol(num1);
-        int ans2 = sol(num2);
-        return Math.max(ans1,ans2);
+       if(nums.length == 0 || nums == null) return 0;
+
+       if(nums.length == 1) return nums[0];
+
+       int robFirst = house(nums,0,nums.length-2);
+       int robSecond = house(nums,1,nums.length-1);
+
+       return Math.max(robFirst,robSecond);
 
     }
-    public static int sol(int[] nums){
-         int n = nums.length;
-        int pre = nums[0];
-        int prev = nums[n-1];
-        for(int i =1;i<n;i++){
-            int pick = nums[i];
-            if(i>1){
-                pick+=prev;
-            }
-            int non = pre;
-            int curr = Math.max(pick,non);
-            prev = pre;
-            pre = curr;
+
+    public int house(int[] nums,int st,int ed){
+        int prev1= 0;
+        int prev2 = 0;
+
+        for(int i = st;i<=ed;i++){
+            int curr = Math.max(prev1,nums[i]+prev2);
+
+            prev2 = prev1;
+            prev1 = curr;
         }
-        return pre;
+
+        return prev1;
     }
 }
